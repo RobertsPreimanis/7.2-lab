eea70d7d1b9807692da542a9596343ce12f96a1d
RobertsPreimanis <roberts.preimanis@edu.rtu.lv>
2025-10-21 10:52:35 +0000

    21.10.25.

--- Main.java
+++ Main.java
@@ -3,19 +3,31 @@
 import java.util.Scanner;
 
 class Main {
-  static public double apr(double a, double guess){
-    if (Math.abs(guess - a/guess) > 0.0001){
-      return apr(a, (guess + a/guess)/2);
-    }else{
-      return guess;
+  public static void num(int count, int[] a, int i, int sum, int countp) {
+    Scanner sc = new Scanner(System.in);
+    if (i < count){
+      a[i] = sc.nextInt();
+      if (a[i] > 0){
+        sum = sum + a[i];  countp ++;
+      }
+      num(count, a, i+1, sum, countp);
+    }
+    else{
+      sc.close();
+      System.out.print("result: ");
+      System.out.printf("%.2f", sum * 1.0 /countp);
     }
   }
-
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    double x = sc.nextDouble();
-    double guess = x/2;
+    int i = 0, sum = 0, countp = 0;
+    System.out.println("251RDC028 Roberts Preimanis");
+    System.out.print("count: ");
+    int count = sc.nextInt();
+    int[] a = new int[count];
+    System.out.println("numbers: ");
+    num(count, a,i, sum, countp);
+
     sc.close();
-    System.out.printf("%.2f", apr(x, guess));
   }
 }
